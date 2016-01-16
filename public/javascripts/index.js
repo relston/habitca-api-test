eb8b8cf56954d52bbb4eeea0c546ee6d379778f2
Ryan Elston <ryanelston@Ryans-MacBook-Pro.local>
2016-01-16 13:47:43 -0800

    better bar chart

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,9 +1,9 @@
 (function (d3){
-	var widthPcnt = 100;
-	var barHeight = 20;
 	var apiUrl = "api";
 
 	function renderChart(data) {
+		var widthPcnt = 100;
+		var barHeight = 20;
 		var max = Math.max.apply(Math,data.habits.map(function(habit){return habit.history.length;}));
 		var d3Scaler = d3.scale.linear()
 			.domain([0, max])
@@ -28,7 +28,49 @@
 			.attr("dy", ".35em")
 			.text(function(d) { return d.text + " " + d.history.length });
 	}
-	d3.json(apiUrl, renderChart);
+	function renderVertChart(data) {
+		var widthPcnt = 100;
+		var height = 400;
+		var barHeight = 300;
+		var barwidth = widthPcnt / data.habits.length;
+		var gutter = .2;
+		
+
+		var max = Math.max.apply(Math,data.habits.map(function(habit){return habit.history.length;}));
+		var barHeightScaler = d3.scale.linear()
+			.domain([0, max])
+			.range([0, barHeight])
+
+		var chart = d3.select('.chart')
+			.attr('width', widthPcnt+"%")
+			.attr('height', height);
+
+		var bar = chart.selectAll("g")
+			.data(data.habits)
+		.enter().append('g')
+			.attr("transform", function (d, i){ return ""; });  // translate("+ (i * barwidth) +"% , 0)
+
+		function barXFunc (d, i) { return (i * barwidth)+"%" ; }
+		function barHeightFunc (d, i){ return barHeightScaler(d.history.length); }
+
+		bar.append("rect")
+			.attr("width", (barwidth-gutter) +"%" )
+			.attr('height', barHeightFunc )
+			.attr('x', barXFunc)
+			.attr('y', function (d, i){ return barHeight - barHeightFunc(d,i); });
+			
+		bar.append('svg')
+			.attr('x', barXFunc )
+			.attr('y',  barHeight )
+			//.attr('width', '100%')
+			//.attr('height', '100%')
+			.append('text')
+				.attr('x', 10)
+				.attr('transform', function (d, i) { return 'rotate(30)'; } )
+				//.attr("dy", "1em")
+				.text(function(d) { return d.history.length + "  "  + d.text  });
+	}
+	d3.json(apiUrl, renderVertChart);
 	
 	var button = document.getElementById('doit');
 	button.addEventListener('click', function(e){
